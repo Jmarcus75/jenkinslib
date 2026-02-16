04aad1c2ad312b5f085ef8e1f09a4d6f0677c243
Jmarcus75 <jmarkenreseau2024@gmail.com>
2026-02-16 13:37:39 +0100

    add variables

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,6 +1,15 @@
 #!/usr/bin/env groovy
-import groovy.json.*
-def call(body) {
-	println("Hello World !!!")
-}
 
+import groovy.json.*
+
+def call(body) {
+    def mapVars             = [:]
+    body.resolveStrategy   = Closure.DELEGATE_FIRST
+    body.delegate          = mapVars
+    body()
+
+		def name = maVars.name
+		def action = maVars.action
+
+	  println( action + " " + name + "!!")
+}
